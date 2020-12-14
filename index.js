3601f32614768a301a13719a1f5a2147a5e84760
gthev <67859478+gthev@users.noreply.github.com>
2020-12-14 15:38:02 +0100

    licence thing

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2020 Gaspard Thévenon
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -257,6 +273,6 @@ var chatInit = function(socket) {
         var res = eval(data);
         Debug.showDebug('DEBUG REQUEST:');
         Debug.showDebug(data);
-        socket.emit('displayAlertMessage', res)
+        //socket.emit('displayAlertMessage', res)
     });
 }
